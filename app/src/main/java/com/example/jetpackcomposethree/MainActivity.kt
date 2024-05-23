80776393f13d6ee6cae722a3f5643a4886ea651c
Moses583 <moisendungu@gmail.com>
2024-05-23 11:39:11 +0300

    Image card creation

--- app/src/main/java/com/example/jetpackcomposethree/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposethree/MainActivity.kt
@@ -1,25 +1,44 @@
 package com.example.jetpackcomposethree
 
+import android.icu.text.CaseMap.Title
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposethree.ui.theme.JetPackComposeThreeTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,29 +46,67 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val painter = painterResource(id = R.drawable.ginger_kitten)
+            val description = "Cute ginger kitten"
+            val contentDescription = "Cute ginger kitten"
             JetPackComposeThreeTheme {
-                Greeting()
+                ImageCard(painter = painter, contentDescription = contentDescription, title = description)
             }
         }
     }
 }
 
 @Composable
-fun Greeting( ) {
-    Column(modifier = Modifier
-        .background(Color.Red)
-        .fillMaxHeight(0.5f)
-        .fillMaxWidth()
-        .padding(top = 50.dp)){
-        Text(text = "Hello")
-        Text(text = "World")
+fun ImageCard(painter: Painter,
+              contentDescription: String,
+              title: String,
+              modifier: Modifier = Modifier)
+{
+    Box(modifier = Modifier
+        .fillMaxWidth(0.5f)
+        .padding(16.dp)){
+        Card(
+            modifier = modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(15.dp),
+        ) {
+            Box(modifier = Modifier.height(200.dp)){
+                Image(
+                    painter = painter,
+                    contentDescription = contentDescription,
+                    contentScale = ContentScale.Inside)
+                Box(modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black
+                            ),
+                            startY = 300f
+                        )
+                    ))
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(12.dp),
+                    contentAlignment = Alignment.BottomStart
+                ){
+                    Text(
+                        style = TextStyle(color = Color.White, fontSize = 16.sp),
+                        text = title
+                    )
+
+                }
+            }
+        }
     }
 }
+
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     JetPackComposeThreeTheme {
-        Greeting()
+        ImageCard(painter = painterResource(id = R.drawable.ginger_kitten), contentDescription = "Cute ginger kitten" , title = "Cute ginger kitten" )
     }
 }
